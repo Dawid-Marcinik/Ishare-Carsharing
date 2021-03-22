ba97f3ce916dc8759db0569d7982650aabdd80d3
Dawid-Marcinik <daw.marcinik@gmail.com>
2021-03-22 12:15:05 +0100

    "Primer Commit - Ishare"

---
+++ Ishare/src/main/java/org/ishare/app/IshareApplication.java
@@ -0,0 +1,13 @@
+package org.ishare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshareApplication.class, args);
+	}
+
+}
---
+++ Ishare/src/test/java/org/ishare/app/IshareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ishare.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
