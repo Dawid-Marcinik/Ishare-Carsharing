9e058a1dab23b404ac26ee2bff784b1f3667ce06
Dawid-Marcinik <daw.marcinik@gmail.com>
2021-03-23 10:05:49 +0100

    "Push Branch David"

---
+++ Ishare/src/main/java/org/ishare/app/domains/Marca.java
@@ -0,0 +1,15 @@
+package org.ishare.app.domains;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Marca {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+}
