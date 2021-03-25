38a5a1983ce37c2d1e0e893cccc67598c4eb9312
aitorfortu <daw1.aitorfortuna@gmail.com>
2021-03-25 13:44:35 +0100

    popo

--- Ishare/src/main/java/org/ishare/app/repositories/EntidadRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ishare.app.repositories;
-
-import org.ishare.app.domains.Entidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntidadRepository extends JpaRepository<Entidad, Long>{
-	public Entidad getByNombreUsuario(String nombreUsuario);
-}
