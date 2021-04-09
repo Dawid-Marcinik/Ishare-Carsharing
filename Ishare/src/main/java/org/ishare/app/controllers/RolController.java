5596e52902d48caffbb2c614a355630fc189447c
aitorfortu <80952256+aitorfortu@users.noreply.github.com>
2021-04-09 17:17:17 +0200

    Validación javascript

--- Ishare/src/main/java/org/ishare/app/controllers/RolController.java
+++ Ishare/src/main/java/org/ishare/app/controllers/RolController.java
@@ -1,6 +1,5 @@
 package org.ishare.app.controllers;
 
-import org.ishare.app.domains.Marca;
 import org.ishare.app.domains.Rol;
 import org.ishare.app.exceptions.DangerException;
 import org.ishare.app.helpers.PRG;
@@ -19,67 +18,70 @@ public class RolController {
 
 	@Autowired
 	private RolRepository rolRepository;
-	
-	//Recuperar
+
+	// Recuperar
 	@GetMapping("r")
-	public String rGet(ModelMap modelo) {
+	public String rGet(final ModelMap modelo) {
 		modelo.put("view", "rol/r");
 		modelo.put("roles", rolRepository.findAll());
-		return ("_t/frame");
+		return "_t/frame";
 	}
-	
-	//Crear
+
+	// Crear
 	@GetMapping("c")
-	public String cGet(ModelMap modelo) {
+	public String cGet(final ModelMap modelo) {
 		modelo.put("view", "rol/c");
-		return ("_t/frame");
+		return "_t/frame";
 	}
+
 	@PostMapping("c")
-	public String cPost(ModelMap modelo, @RequestParam("nombre") String nombre) throws DangerException{
-		
-		if(nombre == "" || nombre == null) {
-			PRG.error("El nombre del rol no puede estar vacío","rol/c");
+	public String cPost(final ModelMap modelo, @RequestParam("nombre") final String nombre) throws DangerException {
+
+		if (nombre == "" || nombre == null) {
+			PRG.error("El nombre del rol no puede estar vacío", "rol/c");
 		}
-		
-		Rol rol = new Rol(nombre);
-		
+
+		final Rol rol = new Rol(nombre);
+
 		try {
 			rolRepository.save(rol);
-		} catch (Exception e) {
-			PRG.error("nombre ya existente","rol/c");
+		} catch (final Exception e) {
+			PRG.error("nombre ya existente", "rol/c");
 		}
-		return("redirect:/rol/r");
+		return "redirect:/rol/r";
 	}
-	
-	//Modificar
+
+	// Modificar
 	@GetMapping("u")
-	public String uGet(ModelMap modelo, @RequestParam("id") Long id) {
+	public String uGet(final ModelMap modelo, @RequestParam("id") final Long id) {
 		modelo.put("rol", rolRepository.getOne(id));
 		modelo.put("view", "rol/u");
-		return ("_t/frame");
+		return "_t/frame";
 	}
+
 	@PostMapping("u")
-	public String uPost(ModelMap modelo, @RequestParam("id") Long id, @RequestParam("nombre") String nombre) throws DangerException{
-		
-		if(nombre == "" || nombre == null) {
-			PRG.error("El nombre del rol no puede estar vacío","rol/u");
+	public String uPost(final ModelMap modelo, @RequestParam("id") final Long id,
+			@RequestParam("nombre") final String nombre) throws DangerException {
+
+		if (nombre == "" || nombre == null) {
+			PRG.error("El nombre del rol no puede estar vacío", "rol/u");
 		}
-			
-		Rol rol = rolRepository.getOne(id);
-			
+
+		final Rol rol = rolRepository.getOne(id);
+
 		try {
 			rol.setNombre(nombre);
 			rolRepository.save(rol);
-		} catch (Exception e) {
-			PRG.error("nombre ya existente","rol/u");
+		} catch (final Exception e) {
+			PRG.error("nombre ya existente", "rol/u");
 		}
-		return("redirect:/rol/r");
+		return "redirect:/rol/r";
 	}
-	
-	//Borrar
-		@PostMapping("d")
-		public String dPost(ModelMap modelo, @RequestParam("id") Long id) {
-			rolRepository.delete(rolRepository.getOne(id));
-			return("redirect:/rol/r");
-		}
+
+	// Borrar
+	@PostMapping("d")
+	public String dPost(final ModelMap modelo, @RequestParam("id") final Long id) {
+		rolRepository.delete(rolRepository.getOne(id));
+		return "redirect:/rol/r";
+	}
 }
