39f0d469ed4d175c0ff45ec5384485c89e0951b1
Marcinik <marcinik@doersdf.com>
2021-05-31 20:49:34 +0200

    "Dios bendito que estás en los cielos sálvanos de Ajax"

--- Ishare/src/main/java/org/ishare/app/controllers/CocheRestController.java
+++ Ishare/src/main/java/org/ishare/app/controllers/CocheRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -27,5 +28,26 @@ public class CocheRestController {
 		List<Coche> listaCoches = cocheRepository.findAll();
 		return new ResponseEntity<>(listaCoches, HttpStatus.OK);
 		
+	}
+	@RequestMapping(value = "/recuperarPorMarca",method = RequestMethod.GET)
+	public ResponseEntity<Object> getCochesByMarca(@RequestParam("nombreMarca") String nombreMarca){
+			
+		List<Coche> listaCoches = cocheRepository.findByModelo_Marca_NombreContaining(nombreMarca);
+		return new ResponseEntity<>(listaCoches, HttpStatus.OK);
+			
+	}
+	@RequestMapping(value = "/recuperarPorModelo",method = RequestMethod.GET)
+	public ResponseEntity<Object> getCochesByModelo(@RequestParam("nombreModelo") String nombreModelo){
+		
+		List<Coche> listaCoches = cocheRepository.findByModelo_NombreContaining(nombreModelo);
+		return new ResponseEntity<>(listaCoches, HttpStatus.OK);
+		
+	}
+	@RequestMapping(value = "/recuperarPorAutonomia",method = RequestMethod.GET)
+	public ResponseEntity<Object> getCochesByAutonomia(@RequestParam("autonomia") Integer autonomia){
+		
+		List<Coche> listaCoches = cocheRepository.findByAutonomiaRestanteGreaterThan(autonomia);
+		return new ResponseEntity<>(listaCoches, HttpStatus.OK);
+			
 	}
 }
